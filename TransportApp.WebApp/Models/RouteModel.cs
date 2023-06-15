1af7580b03daa67632e9de59107e3dcdad341b7f
NathanKalenga <nathankaleng@gmail.com>
2023-06-15 13:00:35 +0300

    Added the localisation in the the transport app

---
+++ TransportApp.WebApp/Models/RouteModel.cs
@@ -0,0 +1,9 @@
+﻿namespace TransportApp.WebApp.Models
+{
+    public class RouteModel
+    {
+        public int Id { get; set; }
+        public string Depart { get; set; }
+        public string End { get; set; } 
+    }
+}
